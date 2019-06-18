3be53ee3ade2365eb4e69d15a90c5de0f5cb0fd9
daniel-momot <daniel.momot@gmail.com>
2019-06-18 09:28:46 +0300

    Первая рабочая версия

---
+++ Must Stay Dead/Library/TCP/TCPExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Net.Sockets;
+
+namespace MSD.Library.TCP
+{
+    /// <summary>
+    /// Данный класс предоставляет функционал для классов TCPSender и TCPReceiver
+    /// </summary>
+    internal static class TCPExtensions
+    {
+        public static void Send(this TcpClient client, string message)
+        {
+            client.Client.Send(message.toByteArray());
+        }
+        public static byte[] toByteArray(this string text)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            using (StreamWriter writer = new StreamWriter(memory))
+            {
+                writer.WriteLine(text);
+                writer.Flush();
+                return memory.ToArray();
+            }
+        }
+
+        public static string ReceiveTextMessage(this TcpClient client)
+        {
+            client.Client.ReceiveTimeout = 100;
+            byte[] data = client.Client.ReceiveAllAvailableData();
+            return data.ToText();
+        }
+        static byte[] ReceiveAllAvailableData(this Socket socket)
+        {
+            //создаем объект для накопления прочитанных данных 
+            using (MemoryStream memory = new MemoryStream())
+            {
+                //создаем буфер для чтения данных 
+                byte[] buffer = new byte[1024];
+                //создаем переменную для хранения количества прочитанных данных за одну опереацию 
+                int len;
+                //читать данные 
+                while ((len = socket.Receive(buffer)) > 0)
+                {
+                    //записываем прочитанные данные в память 
+                    memory.Write(buffer, 0, len);
+                }
+                //инициируем сохранение данных оставшихся в буфере 
+                memory.Flush();
+                //возвращаем двоичный массив, сформированный в памяти 
+                return memory.ToArray();
+            }
+        }
+        static string ToText(this byte[] data)
+        {
+            using (MemoryStream memory = new MemoryStream(data))
+            using (StreamReader reader = new StreamReader(memory))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+
+
+    }
+
+}
---
+++ Must Stay Dead/Library/TCP/TCPSender.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MSD.Library.TCP
+{
+    /// <summary>
+    /// Позволяет отправлять TCP-пакеты
+    /// </summary>
+    public class TCPSender
+    {
+        public event EventHandler<ExceptionArgs> TCPExceptionOccured;
+        private TcpClient client;
+        private readonly IPEndPoint endpoint;
+
+        /// <summary>
+        /// От клиента серверу указывается порт 8010, от сервера клиенту 8020
+        /// </summary>
+        /// <param name="port"></param>
+        public TCPSender(int port)
+        {
+            client = new TcpClient();
+            endpoint = new IPEndPoint(IPAddress.Loopback, port);
+        }
+
+        /// <summary>
+        /// Отправка сообщения по TCP
+        /// </summary>
+        /// <param name="message">Текст сообщения. В рамках данного приложения - команда</param>
+        public void SendMessage(string message)
+        {
+            try
+            {
+                client = new TcpClient();
+                for (int i = 0; i < 10; i++)
+                {
+                    try
+                    {
+                        client.Connect(endpoint);
+                        break;
+                    }
+                    catch (Exception) { }
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    try
+                    {
+                        client.Send(message);
+                        break;
+                    }
+                    catch (Exception) { }
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    try
+                    {
+                        client.Client.Disconnect(false);
+                        client.Dispose();
+                        break;
+                    }
+                    catch (Exception) { }
+                }
+            }
+            catch (Exception e)
+            {
+                TCPExceptionOccured?.Invoke(this, new ExceptionArgs(e.Message, "Ошибка передачи данных"));
+            }
+        }
+
+    }
+
+}
